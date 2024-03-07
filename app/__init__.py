768e8f03fecba0682e4d0c0e9873fd9c8601a090
Stryker Huber <stryker.important@gmail.com>
2024-03-07 14:44:53 -0700

    You're welcome Veronica

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, redirect
 from app.config import Config
+from .shipping_form import ShippingForm
+from map.map import map
 
 app = Flask(__name__)
 
@@ -11,7 +13,21 @@ app.config.from_object(Config)
 def index():
     return "<h1>Package Tracker<h1>"
 
-@app.route("/new_package")
-def newPackage():
-    return render_template("shipping_request.html")
 
+
+@app.route("/new_package", methods=["GET", "POST"])
+def newPackage():
+    form = ShippingForm()
+    if form.validate_on_submit():
+        new_submition = {
+            # "id": len() + 1
+            "sender_name": form.data["sender_name"],
+            "recipient_name": form.data["recipient_name"],
+            "origin": form.data["origin"],
+            "destination": form.data["destination"],
+            "express_shipping": form.data["express_shipping"],
+        }
+        # map.append(new_submition)
+        return redirect('/')
+
+    return render_template("shipping_request.html", form=form)
--- app/shipping_form.py
+++ app/shipping_form.py
@@ -1,11 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField , SelectField, BooleanField
+from wtforms import StringField , SelectField, BooleanField, SubmitField
 from wtforms.validators import InputRequired , DataRequired
+from map.map import map
+
+cities = [(city, city) for city in map.keys()]
 
 class ShippingForm(FlaskForm):
-    sender_name = StringField("Sender Name" , validators=InputRequired())
-    recipient_name = StringField("Recipient Name" , validators=InputRequired())
-    origin = SelectField("Origin" , validators=DataRequired())
-    destination = SelectField("Destination" , validators=DataRequired())
+    # print(cities)
+    sender_name = StringField("Sender Name" , validators=[InputRequired()])
+    recipient_name = StringField("Recipient Name" , validators=[InputRequired()])
+    origin = SelectField("Origin", choices=cities, validators=[DataRequired()])
+    destination = SelectField("Destination", choices=cities, validators=[DataRequired()])
     express_shipping = BooleanField("Express Shipping" , default=False)
-    
+    submit = SubmitField("Submit")
+    cancel = SubmitField('Cancel')
